222972fae26aa6fd7a50e7394c73b29b2d197ea5
Cannen Hanzik <cannenhanzik2@gmail.com>
2022-02-10 21:30:45 -0600

    added username data to a for loop to finish up the project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,11 +4,20 @@ import axios from "axios";
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-axios.get(' https://api.github.com/users/cannen-hanzik')
+const followersArray = [/*'cannen-hanzik',*/ 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'] ;
+
+for (let i = 0; i < followersArray.length; i++){
+  getGit(followersArray[i]);
+}
+
+function getGit(username){
+  axios.get(`https://api.github.com/users/${username}`)
   .then(res => {
     document.querySelector('.cards').appendChild(gitCard(res.data));
   })
   .catch(err => console.error(err))
+}
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -16,6 +25,7 @@ axios.get(' https://api.github.com/users/cannen-hanzik')
 
     Skip to STEP 3 (line 34).
 */
+
 function gitCard(information) {
   const card = document.createElement('div');
   const img = document.createElement('img');
@@ -75,7 +85,7 @@ function gitCard(information) {
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
